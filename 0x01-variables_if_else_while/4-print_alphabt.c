8d922c3f272bf993c92b37f511314b9d1f2b09f0
Sithembile <sthezmtuzela@yahoo.com>
2023-04-22 11:27:56 -0700

    do not print q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabet.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabets[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		if (alphabets[i] != 'q' || alphabets[i] != 'e')
+		{
+			putchar(alphabets[i]);
+		}
+	}
+	putchar('\n');
+	return (0);
+}

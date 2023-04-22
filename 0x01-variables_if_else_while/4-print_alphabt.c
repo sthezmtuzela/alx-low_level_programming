93c3364967de0b4c7df1c672d82e13521e4f5bc6
Sithembile <sthezmtuzela@yahoo.com>
2023-04-22 12:00:20 -0700

    fix

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	char alphabets[26] = "abcdefghijklmnopqrstuvwxyz";
+	char alphabets[24] = "abcdfghijklmnoprstuvwxyz";
 	int i;
 
-	for (i = 0; i < 26; i++)
+	for (i = 0; i < 24; i++)
 	{
 		if (alphabets[i] != 'q' || alphabets[i] != 'e' )
 		{

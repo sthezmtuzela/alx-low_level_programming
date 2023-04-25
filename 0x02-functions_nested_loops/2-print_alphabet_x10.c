ca0771e60b65d1f64d845b36ba26be4f1500254c
Sithembile <sthezmtuzela@yahoo.com>
2023-04-25 14:12:53 -0700

    10 times alphabets

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *print_alphabet - print alphabet
+ *
+ * Return - void
+ */
+void print_alphabet_x10(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		char i = 'a';
+		while (i <= 'z')
+		{
+			putchar(i);
+			i++;
+		}
+
+		putchar('\n');
+		num++;
+	}
+}

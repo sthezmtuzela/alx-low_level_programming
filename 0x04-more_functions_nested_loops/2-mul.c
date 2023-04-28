660bbfd6023e7c43a33749173a9949d7309915ec
Sithembile <sthezmtuzela@yahoo.com>
2023-04-28 08:12:30 -0700

    fix

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,10 +2,9 @@
 
 /**
  * mul - Multiplies two numbers
- *@a - First number to multiply
- *@b - Second number to multiply
- *
- *Return - Product of a and b
+ *@a: First number to multiply
+ *@b: Second number to multiply
+ *Return: Product of a and b
  */
 int mul(int a, int b)
 {

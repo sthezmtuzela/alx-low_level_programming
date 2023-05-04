00819de605d972fe8f85c648e8bfb3ed92df22ad
Sithembile <sthezmtuzela@yahoo.com>
2023-05-04 11:15:33 -0700

    capitalize

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalizes all words of a string
+ * @s: input string
+ *
+ * Return: string
+ */
+char *cap_string(char *s)
+{
+	int a = 0, i;
+
+	return (s);
+}
+

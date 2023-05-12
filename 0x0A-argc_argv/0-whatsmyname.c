8de650ce0b74bda4a486c6f4a11a25c36e62b00d
Sithembile <sthezmtuzela@yahoo.com>
2023-05-12 11:52:19 -0700

    main arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+  * main - Prints its name
+  * @argc: argument count
+  * @argv: argument value
+  *
+  * Return: Always zero
+  */
+int main(int argc, char *argv[])
+{
+	printf("My name is: %s\n", argv[0]);
+
+	return (0);
+}

4688955979d7b46c936176c3dd4d02d7a1c9afdc
Sithembile <sthezmtuzela@yahoo.com>
2023-06-06 12:28:43 -0700

    fix

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -4,6 +4,29 @@
 #include "lists.h"
 
 /**
+ * free_listp - frees a linked list
+ * @head: head of a list.
+ *
+ * Return: no return.
+ */
+void free_listp(listp_t **head)
+{
+	listp_t *temp;
+	listp_t *curr;
+
+	if (head != NULL)
+	{
+		curr = *head;
+		while ((temp = curr) != NULL)
+		{
+			curr = curr->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
+
+/**
  * print_listint_safe - prints a linked list.
  * @head: head of a list.
  *
@@ -44,5 +67,6 @@ size_t print_listint_safe(const listint_t *head)
 		nnodes++;
 	}
 
+	free_listp(&hptr);
 	return (nnodes);
 }
